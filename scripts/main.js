9ae0d0ee59de4e0830b3a5a403e99f66c27f448e
miguel <miguel.aristizabalp.col@upb.edu.co>
2025-02-09 00:38:31 -0500

    MenuHamburguesa

--- scripts/main.js
+++ scripts/main.js
@@ -56,3 +56,16 @@ function nextSlide() {
     slideIndex++;
     showSlide(slideIndex);
 }
+
+
+// Barra de navegación
+
+// Seleccionamos el botón y la lista de navegación
+const navToggle = document.querySelector('.nav-toggle');
+const navUl = document.querySelector('nav ul');
+
+// Al hacer clic en el botón, alternamos la clase "active" en el menú
+navToggle.addEventListener('click', () => {
+    navUl.classList.toggle('active');
+});
+
