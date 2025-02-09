07d65055f6efc92c9b79c8e3d00f48ba050ec8db
miguel <miguel.aristizabalp.col@upb.edu.co>
2025-02-08 22:28:39 -0500

    Formulario/BotónFlotante

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,58 @@
+// Selecciona todas las imágenes dentro del contenedor con la clase slides
 const slides = document.querySelectorAll(".slides img");
+
+// Índice para conocer la imagen actual
 let slideIndex = 0;
+
+// contador de tiempo que cambia las imagenes
 let intervalId = null;
 
 document.addEventListener("DOMContentLoaded", initializeSlider);
 
-function initializeSlider(){
-    if(slides.length > 0){
+function initializeSlider() {
+
+    if (slides.length > 0) {
+        // Muestra la primera diapositiva al cargar la página
         slides[slideIndex].classList.add("displaySlide");
+
+        // Inicia un temporizador para cambiar de diapositiva cada 5 segundos
         intervalId = setInterval(nextSlide, 5000);
     }
 }
 
-function showSlide(index){
-    if(index >= slides.length){
+// Función para mostrar una diapositiva específica según su índice
+function showSlide(index) {
+
+    if (index >= slides.length) {
         slideIndex = 0;
     }
-    else if(index < 0){
+
+    else if (index < 0) {
         slideIndex = slides.length - 1;
     }
+
 
     slides.forEach(slide => {
         slide.classList.remove("displaySlide");
     });
+
+
     slides[slideIndex].classList.add("displaySlide");
 }
 
-function prevSlide(){
+// Función para retroceder a la diapositiva anterior
+function prevSlide() {
+
     clearInterval(intervalId);
+
+
     slideIndex--;
     showSlide(slideIndex);
 }
 
-function nextSlide(){
+// Función para avanzar a la siguiente diapositiva
+function nextSlide() {
+
     slideIndex++;
     showSlide(slideIndex);
 }
